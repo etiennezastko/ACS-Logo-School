75504f687ff61f723106555c3ec451f1cf524a73
etiennezastko <etiennezastko@hotmail.fr>
2021-04-06 13:27:56 +0200

    logo ACS-School

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="parents">
+            <p class="titre"><span class="underline">&#60ACCESS
+                    <span class="code">
+
+                        CODE</span>&#62</span>
+                <br><span class="school">SCHOOL</span>
+
+            </p>
+
+
+        </div>
+    </div>
+    </div>
+
+
+</body>
+
+</html>
